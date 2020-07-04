b8c95f6e13e69bb18834d6a0c92d5657d6693639
Peter Kertesz <kerteszpetr@gmail.com>
2020-07-04 08:08:20 +0100

    Connecting to Mongo DB database.

--- server/app.js
+++ server/app.js
@@ -1,6 +1,24 @@
 const express = require('express');
-const { ModuleResolutionKind } = require('typescript');
+const mongoose = require('mongoose');
+
 const app = express();
+
+mongoose
+	.connect(
+		'mongodb+srv://Wo0dyStars:' +
+			process.env.MONGODB_PASSWORD +
+			'@cluster0.blrzn.mongodb.net/<dbname>?retryWrites=true&w=majority',
+		{
+			useUnifiedTopology: true,
+			useNewUrlParser: true
+		}
+	)
+	.then(() => {
+		console.log('You are now connected to the Mongo DB database.');
+	})
+	.catch(() => {
+		console.log('Your attempt to connect to MongoDB database has been failed.');
+	});
 
 app.get('/', (req, res, next) => {
 	res.send('here');
